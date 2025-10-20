154a359831fc3aced75caf2de0a91b2e491e4a1e
faisalljavid <faisalljavid@gmail.com>
2025-10-20 12:40:31 +0530

    Add Landing Page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,18 @@
+import { Header } from "@/features/home/components/header"
+import React from "react"
+import { Footer } from "react-day-picker"
+
+export default function HomeLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <>
+            <Header />
+            <main className="z-20 relative w-full pt-0 md:pt-0">
+                {children}
+            </main>
+        </>
+    )
+}
