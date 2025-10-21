e5c24ba341a86e654f67b9ea8637f84c6f5ca880
faisalljavid <faisalljavid@gmail.com>
2025-10-21 17:16:56 +0530

    Add Zustand for state management and implement file explorer components in playground

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -5,18 +5,39 @@ import { SidebarInset, SidebarTrigger } from '@/components/ui/sidebar'
 import { useParams } from 'next/navigation'
 import { Separator } from '@/components/ui/separator'
 import { usePlayground } from '@/features/playground/hooks/usePlayground'
+import TemplateFileTree from '@/features/playground/components/template-file-tree'
+import { useFileExplorer } from '@/features/playground/hooks/useFileExplorer'
 
 const page = () => {
     const { id } = useParams<{ id: string }>()
     const { playgroundData, templateData, isLoading, error, saveTemplateData } = usePlayground(id)
-
-    console.log(templateData)
-    console.log("hello", playgroundData)
+    const {
+        activeFileId,
+        closeAllFiles,
+        openFile,
+        closeFile,
+        editorContent,
+        updateFileContent,
+        handleAddFile,
+        handleAddFolder,
+        handleDeleteFile,
+        handleDeleteFolder,
+        handleRenameFile,
+        handleRenameFolder,
+        openFiles,
+        setTemplateData,
+        setActiveFileId,
+        setPlaygroundId,
+        setOpenFiles,
+    } = useFileExplorer()
 
     return (
         <div>
             <>
-                {/* TODO: Template File Tree */}
+                <TemplateFileTree
+                    data={templateData!}
+                />
+
                 <SidebarInset>
                     <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4'>
                         <SidebarTrigger className='-ml-1' />
